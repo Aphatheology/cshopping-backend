a1ee0a77b7aba39d41e5344f188698917c7c925a
Aphatheology <aphatheology@gmail.com>
2024-02-15 23:57:38 +0100

    getting started

---
+++ src/main/java/com/aphatheology/cshoppingbackend/CshoppingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aphatheology.cshoppingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CshoppingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CshoppingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aphatheology/cshoppingbackend/CshoppingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aphatheology.cshoppingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CshoppingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
